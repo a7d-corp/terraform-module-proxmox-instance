9280fe765eb967d95f5e4c0f04b9a3510c2e724b
Simon Weald <glitchcrab-github@simonweald.com>
2021-07-03 18:48:22 +0100

    add descriptions for all variables and some more defaults

--- variables.tf
+++ variables.tf
@@ -1,52 +1,68 @@
 variable "pve_instance_name" {
-  type = string
+  description = "Name of the instance."
+  type        = string
 }
 
 variable "pve_instance_description" {
-  type = string
+  description = "Description of the instance."
+  type        = string
 }
 
 variable "vmid" {
-  type = number
+  description = "ID of the VM (must be unique)."
+  type        = number
 }
 
 variable "clone" {
-  type = string
+  description = "Name of the template to clone."
+  type        = string
 }
 
 variable "full_clone" {
-  type = bool
+  default     = false
+  description = "Create a full clone (if false, a linked clone will be created)."
+  type        = bool
 }
 
 variable "qemu_agent" {
-  type = number
+  default     = 0
+  description = "Enable QEMU guest agent (must be installed in the template). Set to `1` to enable or `0` to disable."
+  type        = number
 }
 
 variable "target_node" {
-  type = string
+  description = "Name of the node to assign the instance to."
+  type        = string
 }
 
 variable "resource_pool" {
-  type = string
+  description = "Name of the resource pool the assign the instance to."
+  type        = string
 }
 
 variable "cores" {
-  type = number
+  description = "Number of cores to allocate."
+  type        = number
 }
 
 variable "sockets" {
-  type = number
+  description = "Number of sockets to allocate."
+  type        = number
 }
 
 variable "memory" {
-  type = number
+  description = "Amount of memory to allocate."
+  type        = number
 }
 
 variable "boot" {
-  type = string
+  default     = "cdn"
+  description = "Boot order for the instance."
+  type        = string
 }
 
 variable "network_interfaces" {
+  description = "List of objects representing instance interface configuration."
   type = list(object({
     model   = string
     bridge  = string
@@ -56,33 +72,45 @@ variable "network_interfaces" {
 }
 
 variable "os_type" {
-  type = string
+  description = "Type of OS for preprovisioning."
+  type        = string
 }
 
 variable "snippet_dir" {
-  type = string
+  default     = "snippets"
+  description = "Name of the snippet subdirectory."
+  type        = string
 }
 
 variable "snippet_file_base" {
-  type = string
+  default     = "/var/lib/vz"
+  description = "Path to the directory containing the snippet_dir."
+  type        = string
 }
 
 variable "citemplate_storage" {
-  type = string
+  default     = "local"
+  description = "Name of the storage containing the Cloudinit snippets."
+  type        = string
 }
 
 variable "cloudinit_cdrom_storage" {
-  type = string
+  default     = "local-lvm"
+  description = "Name of the storage to create the Cloudinit image in."
+  type        = string
 }
 
 variable "instance_domain" {
-  type = string
+  description = "Domain name to use."
+  type        = string
 }
 
 variable "searchdomain" {
-  type = string
+  description = "DNS searchdomain to use."
+  type        = string
 }
 
 variable "nameserver" {
-  type = string
+  description = "Nameserver to use in the instance."
+  type        = string
 }
