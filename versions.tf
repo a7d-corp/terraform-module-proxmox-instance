fa22cb3f630846df027c825f93c0faeb6faad94c
Simon Weald <glitchcrab-github@simonweald.com>
2021-08-31 21:37:49 +0100

    enable experimental optional vars

--- versions.tf
+++ versions.tf
@@ -1,8 +1,15 @@
 terraform {
+  # minimum required version for experimental features.
+  required_version = "0.14.0"
+
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
       version = ">= 2.6.9"
     }
   }
+
+  # enable experimental optional variables.
+  # TODO: remove when no longer experimental.
+  experiments = [module_variable_optional_attrs]
 }
