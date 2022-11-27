d9ca5da2cc75982a0069b926035d2784edb3fe41
Simon Weald <7832341+glitchcrab@users.noreply.github.com>
2022-11-27 09:42:28 +0000

    remove feature toggle for experimental attributes (#9)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Removed
+
+- Remove feature toggle for experimental attributes.
+
 ## [1.0.0] - 2022-11-26
 
 ### Added
--- versions.tf
+++ versions.tf
@@ -1,5 +1,4 @@
 terraform {
-  # minimum required version for experimental features.
   required_version = ">= 0.14.0"
 
   required_providers {
@@ -8,8 +7,4 @@ terraform {
       version = ">= 2.6.9"
     }
   }
-
-  # enable experimental optional variables.
-  # TODO: remove when no longer experimental.
-  experiments = [module_variable_optional_attrs]
 }
